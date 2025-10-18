600c4477d21946ef516f99da68f6b335a57a460d
Patrick <pdcamargo@hotmail.com>
2025-10-18 15:19:24 -0300

    update animation update system

--- packages/webgpu-renderer/src/ecs/systems/animation-update.ts
+++ packages/webgpu-renderer/src/ecs/systems/animation-update.ts
@@ -1,6 +1,5 @@
 import { sys, Time } from "@atlas/core";
 import { AnimatedSprite } from "../../renderer/AnimatedSprite";
-import { SceneGraph } from "../../renderer/SceneGraph";
 
 /**
  * System that updates all AnimatedSprite instances in scene graphs
@@ -11,44 +10,47 @@ export const animationUpdateSystem = sys(({ commands, events }) => {
   const deltaTime = time.deltaTime;
 
   // Get all scene graphs
-  const sceneGraphs = commands.query(SceneGraph).all();
+  const animatedSprites = commands.query(AnimatedSprite).all();
 
   // Traverse each scene graph to find AnimatedSprite instances
-  for (const [entity, sceneGraph] of sceneGraphs) {
-    sceneGraph.traverse((node) => {
-      if (!(node instanceof AnimatedSprite)) {
-        return;
-      }
+  for (const [entity, node] of animatedSprites) {
+    // Update the animation and get event flags
+    const eventFlags = node.updateAnimation(deltaTime);
 
-      // Update the animation and get event flags
-      const eventFlags = node.updateAnimation(deltaTime);
+    // Get current animation to check for event classes
+    const currentAnimation = node.getCurrentAnimation();
+    if (!currentAnimation) {
+      return;
+    }
 
-      // Get current animation to check for event classes
-      const currentAnimation = node.getCurrentAnimation();
-      if (!currentAnimation) {
-        return;
-      }
+    // Fire start event if configured and flagged
+    if (eventFlags.shouldFireStart && currentAnimation.onStart) {
+      const EventClass = currentAnimation.onStart;
+      const eventInstance = new EventClass(
+        entity,
+        node.getCurrentAnimationName()
+      );
+      events.writer(EventClass).send(eventInstance);
+    }
 
-      // Fire start event if configured and flagged
-      if (eventFlags.shouldFireStart && currentAnimation.onStart) {
-        const EventClass = currentAnimation.onStart;
-        const eventInstance = new EventClass(entity, node.getCurrentAnimationName());
-        events.writer(EventClass).send(eventInstance);
-      }
+    // Fire end event if configured and flagged
+    if (eventFlags.shouldFireEnd && currentAnimation.onEnd) {
+      const EventClass = currentAnimation.onEnd;
+      const eventInstance = new EventClass(
+        entity,
+        node.getCurrentAnimationName()
+      );
+      events.writer(EventClass).send(eventInstance);
+    }
 
-      // Fire end event if configured and flagged
-      if (eventFlags.shouldFireEnd && currentAnimation.onEnd) {
-        const EventClass = currentAnimation.onEnd;
-        const eventInstance = new EventClass(entity, node.getCurrentAnimationName());
-        events.writer(EventClass).send(eventInstance);
-      }
-
-      // Fire loop event if configured and flagged
-      if (eventFlags.shouldFireLoop && currentAnimation.onLoop) {
-        const EventClass = currentAnimation.onLoop;
-        const eventInstance = new EventClass(entity, node.getCurrentAnimationName());
-        events.writer(EventClass).send(eventInstance);
-      }
-    });
+    // Fire loop event if configured and flagged
+    if (eventFlags.shouldFireLoop && currentAnimation.onLoop) {
+      const EventClass = currentAnimation.onLoop;
+      const eventInstance = new EventClass(
+        entity,
+        node.getCurrentAnimationName()
+      );
+      events.writer(EventClass).send(eventInstance);
+    }
   }
 }).label("WebgpuRenderer::AnimationUpdate");
